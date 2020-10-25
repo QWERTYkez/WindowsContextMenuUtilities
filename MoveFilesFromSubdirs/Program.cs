0a81cd9b43f19c27de0ff0be99381f99c16eb239
Fantom TM <QWERTYkez@outlook.com>
2020-10-25 19:08:09 +0700

    MoveFilesFromSubdirs Complete

--- MoveFilesFromSubdirs/Program.cs
+++ MoveFilesFromSubdirs/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 
 namespace MoveFilesFromSubdirs
 {
@@ -6,7 +8,43 @@ namespace MoveFilesFromSubdirs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 0)
+            {
+                if (args[0] == "-GetInfo")
+                {
+                    Console.WriteLine("Извлечь файлы из подкаталогов");
+                    Console.WriteLine("into");
+                }
+
+                if (args[0] == "-Path")
+                {
+                    var dir = new DirectoryInfo(args[1]);
+
+                    GatFiles(dir.FullName, dir);
+                }
+            }
+        }
+
+        static void GatFiles(string BasePath, DirectoryInfo from)
+        {
+            foreach (var d in from.GetDirectories())
+            {
+                foreach (var f in d.GetFiles())
+                {
+                    var fn = d.FullName.Replace($"{BasePath}\\", "");
+                    string nn = "";
+                    foreach (string ss in fn.Split('\\')) nn += ss + '.';
+
+                    f.MoveTo($@"{BasePath}\{nn}{f.Name}");
+                }
+                GatFiles(BasePath, d);
+
+                while (Directory.Exists(d.FullName))
+                {
+                    d.Delete();
+                    Thread.Sleep(100);
+                }
+            }
         }
     }
 }

8779dd01a8535c84228c0e9f27df804d08836d31
Fantom TM <QWERTYkez@outlook.com>
2020-10-25 18:40:44 +0700

    Origin

---
+++ ContextUtilsInstaller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContextUtilsInstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DeleteEmptyDirs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeleteEmptyDirs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

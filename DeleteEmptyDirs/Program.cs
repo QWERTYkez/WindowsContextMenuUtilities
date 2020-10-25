b9eb8507405f939f532ebd9ccd0093b64409cf7c
Fantom TM <QWERTYkez@outlook.com>
2020-10-25 19:44:07 +0700

    DeleteEmptyDirs Complete

--- DeleteEmptyDirs/Program.cs
+++ DeleteEmptyDirs/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 
 namespace DeleteEmptyDirs
 {
@@ -6,7 +8,33 @@ namespace DeleteEmptyDirs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 0)
+            {
+                if (args[0] == "-GetInfo")
+                {
+                    Console.WriteLine("Удалить пустые подкаталоги");
+                    Console.WriteLine("into");
+                }
+
+                if (args[0] == "-Path")
+                    DeleteDirs(new DirectoryInfo(args[1]));
+            }
+        }
+
+        static void DeleteDirs(DirectoryInfo from)
+        {
+            foreach (var d in from.GetDirectories())
+            {
+                if (d.GetFiles().Length == 0)
+                {
+                    while (Directory.Exists(d.FullName))
+                    {
+                        d.Delete();
+                        Thread.Sleep(100);
+                    }
+                }
+                else DeleteDirs(d);
+            }
         }
     }
 }

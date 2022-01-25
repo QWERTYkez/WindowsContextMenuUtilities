5d219d5de01ce05cb1d3fed6eb6e9728788f2371
QWERTYkez <QWERTYkez@outlook.com>
2022-01-26 04:18:11 +0700

    Release

--- ExtractFilesTree/Program.cs
+++ ExtractFilesTree/Program.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
 using System.Security.Principal;
 using System.Threading;
+using Microsoft.Win32;
 
-namespace DeleteEmptyDirs;
+namespace ExtractFilesTree;
 
 class Program
 {
@@ -14,21 +14,25 @@ class Program
         if (args.Length > 0)
         {
             if (args[0] == "-Path")
-                DeleteDirs(new DirectoryInfo(args[1]));
+            {
+                var dir = new DirectoryInfo(args[1]);
+
+                GatFiles(dir.FullName, dir);
+            }
         }
         if (args.Length == 0)
         {
             if (new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator))
             {
                 Console.WriteLine("Подключить - [1], Отключить - [2]");
-                switch (Console.ReadLine())
+                switch (Console.ReadLine()) 
                 {
-                    case "1":
+                    case "1": 
                         {
-                            using (var root = Registry.ClassesRoot.OpenSubKey("Directory", true).OpenSubKey("Background", true).OpenSubKey("Shell", true).CreateSubKey("DeleteEmptyDirs", true))
+                            using (var root = Registry.ClassesRoot.OpenSubKey("Directory", true).OpenSubKey("Background", true).OpenSubKey("Shell", true).CreateSubKey("ExtractFilesTree", true))
                             {
                                 //header
-                                root.SetValue(null, "DeleteEmptyDirs");
+                                root.SetValue(null, "ExtractFilesTree");
                                 using (var cmd = root.CreateSubKey("command", true))
                                 {
                                     //command
@@ -41,32 +45,42 @@ class Program
                         return;
                     case "2":
                         {
-                            Registry.ClassesRoot.OpenSubKey("Directory", true).OpenSubKey("Background", true).OpenSubKey("Shell", true).DeleteSubKeyTree("DeleteEmptyDirs");
+                            Registry.ClassesRoot.OpenSubKey("Directory", true).OpenSubKey("Background", true).OpenSubKey("Shell", true).DeleteSubKeyTree("ExtractFilesTree");
                             Console.WriteLine("Успешно!!");
                             Console.ReadLine();
                         }
                         return;
                 }
             }
-            Console.WriteLine("Удалить пустые каталоги");
+            Console.WriteLine("Развернуть древо файлов");
             Console.WriteLine("Введите путь");
-            DeleteDirs(new DirectoryInfo($"{Console.ReadLine()}"));
+            var dir = new DirectoryInfo($"{Console.ReadLine()}");
+            GatFiles(dir.FullName, dir);
         }
     }
 
-    static void DeleteDirs(DirectoryInfo from)
+    static void GatFiles(string BasePath, DirectoryInfo from)
     {
+        Console.WriteLine("Обработка");
+
         foreach (var d in from.GetDirectories())
         {
-            if (d.GetDirectories().Length > 0) DeleteDirs(d);
-            if (d.GetFiles().Length == 0 && d.GetDirectories().Length == 0)
+            foreach (var f in d.GetFiles())
             {
-                while (Directory.Exists(d.FullName))
-                {
-                    d.Attributes = FileAttributes.Normal;
-                    d.Delete();
-                    Thread.Sleep(100);
-                }
+                var fn = d.FullName.Replace($"{BasePath}\\", "");
+                string nn = "";
+                foreach (string ss in fn.Split('\\')) nn += ss + '.';
+
+                f.Attributes = FileAttributes.Normal;
+                f.MoveTo($@"{BasePath}\{nn}{f.Name}");
+            }
+            GatFiles(BasePath, d);
+
+            while (Directory.Exists(d.FullName))
+            {
+                d.Attributes = FileAttributes.Normal;
+                d.Delete();
+                Thread.Sleep(100);
             }
         }
     }
